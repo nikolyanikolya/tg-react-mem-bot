87d05c7e934601833ec0cb15b6f63ef2e4642af1
Ignatov Nikolay <nickigna610@gmail.com>
2023-03-05 16:47:30 +0300

    add accessible image

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       work
       <button onClick={onClose}>Закрыть приложение</button>
-        <Images data={["https://www.google.com/search?q=%D0%BA%D0%BE%D1%82+%D0%BA%D0%B0%D1%80%D1%82%D0%B8%D0%BD%D0%BA%D0%B0&client=ubuntu&hs=zDD&channel=fs&sxsrf=AJOqlzXmbbdf9TRk0c4zmpVzdtp_YJYr3w:1678022888359&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjG472x8sT9AhWvtIsKHf2nAgwQ_AUoAXoECAEQAw&biw=1848&bih=968&dpr=1#imgrc=XlGkFXEyc7T5wM"]}/>
+      <Images data={["https://api.memegen.link/images/panik-kalm-panik.png"]}/>
     </div>
   );
 }
